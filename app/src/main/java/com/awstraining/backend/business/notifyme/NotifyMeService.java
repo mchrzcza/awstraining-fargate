9c4c7ea54d77a5d4f2ce8fdc9a43f353b4459925
Marcin Chrzczanowicz <external.marcin.chrzczanowicz@kuehne-nagel.com>
2024-06-06 13:47:51 +0200

    implement notification

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotifyMeService {
 
+    private final MessageSender messageSender;
+    private final Translator translator;
+    private final Sentiment sentimentDetector;
+
     // TODO: lab1
     //  1. Inject MessageSender.
     // TODO lab2
@@ -12,12 +16,15 @@ public class NotifyMeService {
     // TODO lab3
     //  1. Inject sentiment detector
 //    @Autowired
-    public NotifyMeService() {
-
+    public NotifyMeService(MessageSender messageSender, Translator translator, Sentiment sentimentDetector) {
+        this.messageSender = messageSender;
+        this.translator = translator;
+        this.sentimentDetector = sentimentDetector;
     }
-    
+
     public String notifyMe(NotifyMeDO notifyMe) {
-      
+      final String text = notifyMe.text();
+      messageSender.send(text);
         // TODO: lab1
         //  1. Send text using sender.
         //  2. Return sent message.
@@ -27,7 +34,7 @@ public class NotifyMeService {
         // TODO: lab3
         //  1. Detect sentiment of translated message.
         //  2. Change sending of text to "setiment: translated text" and return it.
-        return "This service is not available.";
+        return text;
     }
-    
+
 }
--- app/src/main/java/com/awstraining/backend/business/notifyme/adapter/MessageSnsAWSSender.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/adapter/MessageSnsAWSSender.java
@@ -1,8 +1,12 @@
 package com.awstraining.backend.business.notifyme.adapter;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
 import com.awstraining.backend.business.notifyme.MessageSender;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,18 +14,24 @@ public class MessageSnsAWSSender implements MessageSender {
 
     private static final Logger LOGGER = LogManager.getLogger(MessageSnsAWSSender.class);
 
-    // TODO: lab1
-    //  1. Inject AWS AmazonsSNS from configuration SNSConfig.
-    //  2. Make sure that you created new value in parameter store with arn of sns topic.
-    //  3. Inject parameter with @Value annotation through constructor.
-//    @Autowired
-    public MessageSnsAWSSender() {
+    private final AmazonSNS amazonSNS;
+
+    @Value("${notification.topicarn}")
+    String topicArn;
+
+    public MessageSnsAWSSender(AmazonSNS amazonSNS) {
+        this.amazonSNS = amazonSNS;
     }
 
     @Override
     public void send(String text) {
+        final PublishRequest publishRequest = new PublishRequest(topicArn, text);
+        final PublishResult publishResult = amazonSNS.publish(publishRequest);
+        LOGGER.info("Message was sent to topic: '{}' with id '{}'.",
+                topicArn,
+                publishResult.getMessageId());
         // TODO: lab1
-        //  1. Create publish request.
+        //  1. Create publishResult request.
         //  2. Publish request with sns.
         //  3. Log information about successful sent message to topic with topic arn and message id.
     }
